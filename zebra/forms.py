cbf344b509872641db85b48a4899ba12b10c8710
Eric Powers <eric@mrpowers.com>
2017-11-13 22:10:42 -0800

    fix iteritems

--- zebra/forms.py
+++ zebra/forms.py
@@ -23,7 +23,7 @@ class StripePaymentForm(CardForm):
         self.fields['card_cvv'].label = "Card CVC"
         self.fields['card_cvv'].help_text = "Card Verification Code; see rear of card."
         months = [ (m[0], u'%02d - %s' % (m[0], unicode(m[1])))
-                    for m in sorted(MONTHS.iteritems()) ]
+                    for m in sorted(MONTHS.items()) ]
         self.fields['card_expiry_month'].choices = months
 
     card_number = forms.CharField(required=False, max_length=20,
@@ -31,6 +31,6 @@ class StripePaymentForm(CardForm):
     card_cvv = forms.CharField(required=False, max_length=4,
         widget=NoNameTextInput())
     card_expiry_month = forms.ChoiceField(required=False, widget=NoNameSelect(),
-        choices=MONTHS.iteritems())
+        choices=MONTHS.items())
     card_expiry_year = forms.ChoiceField(required=False, widget=NoNameSelect(),
         choices=options.ZEBRA_CARD_YEARS_CHOICES)
